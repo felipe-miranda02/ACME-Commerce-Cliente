d328b497fbb66da7e75a882fa2dbe5e37ad11281
felipe-miranda02 <felipemiranda20023@gmail.com>
2023-11-27 23:23:56 -0300

    Ruta api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: `https://acmecommerce20231127214815.azurewebsites.net/api/`,
+  base_url: `https://acme-ecommerce-api123456.azurewebsites.net/api/`,
   pageSize: 10,
 };
