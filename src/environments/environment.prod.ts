5f42445c48810e4a0564acd805c6161084ab2a8d
felipe-miranda02 <felipemiranda20023@gmail.com>
2023-11-27 22:14:07 -0300

    Ruta de deploy de API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: `https://localhost:7255/api/`,
+  base_url: `https://acmecommerce20231127214815.azurewebsites.net/api/`,
   pageSize: 10,
 };
